03754bd8657f44e8ff536dda14643c2993e0804a
ahoang2502 <athoang252@gmail.com>
2023-10-29 13:19:52 +1100

    trash banner + navbar delete

--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -7,6 +7,8 @@ import { MenuIcon } from "lucide-react";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import Title from "./Title";
+import Banner from "./Banner";
+import Menu from "./Menu";
 
 interface NavbarProps {
 	isCollapsed: boolean;
@@ -19,11 +21,12 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 	const document = useQuery(api.documents.getDocumentById, {
 		documentId: params.documentId as Id<"documents">,
 	});
-	if (document === undefined) return (
-		<nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex">
-			<Title.Skeleton />
-		</nav>
-	);
+	if (document === undefined)
+		return (
+			<nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex">
+				<Title.Skeleton />
+			</nav>
+		);
 	if (document === null) return null;
 
 	return (
@@ -38,8 +41,14 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 				)}
 				<div className="flex items-center justify-between w-full">
 					<Title initialData={document} />
+
+					<div className="flex items-center gap-x-2">
+						<Menu documentId={document._id} />
+					</div>
 				</div>
 			</nav>
+
+			{document.isArchived && <Banner documentId={document._id} />}
 		</>
 	);
 };
